9a8578dbcbb0b3a46ca279f4714894a11ae5235e
Paul Kalupnieks <paul@unbounce.com>
2015-06-20 11:33:33 -0700

    Initial Commit of Xcode Project create

---
+++ WriterTest/WriterTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WriterTest
+//
+//  Created by Paul Kalupnieks on 2015-06-20.
+//  Copyright (c) 2015 Paul Kalupnieks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
