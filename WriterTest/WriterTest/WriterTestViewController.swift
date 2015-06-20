c0ef3b579471db4f03719eba6908ae4d41edb0fc
Paul Kalupnieks <paul@unbounce.com>
2015-06-20 12:16:56 -0700

    Re set up views

---
+++ WriterTest/WriterTest/WriterTestViewController.swift
@@ -0,0 +1,14 @@
+//
+//  WriterTestViewController.swift
+//  WriterTest
+//
+//  Created by Paul Kalupnieks on 2015-06-20.
+//  Copyright (c) 2015 Paul Kalupnieks. All rights reserved.
+//
+
+import UIKit
+
+class WriterTestViewController: UIViewController {
+
+
+}
